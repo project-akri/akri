a265b976ccec5d4e553ea6bf4bd09a97bf4a3b6d
bfjelds <bfjelds@microsoft.com>
2020-10-21 09:46:53 -0700

    add slot reconciliation check to end to end test

--- test/run-end-to-end.py
+++ test/run-end-to-end.py
@@ -96,6 +96,10 @@ def do_test():
         os.system('sudo {} get pods,services,akric,akrii --show-labels'.format(kubectl_cmd))
         return False
 
+    # Check Akri slot reconiliation logs for success
+    print("Check logs for Agent slot-reconciliation for pod {}".format(shared_test_code.agent_pod_name))
+    os.system('sudo {} logs $(sudo {} get pods | grep agent | awk \'{{print $1}}\') | grep "get_node_slots - crictl called successfully" | wc -l | grep -v 0'.format(kubectl_cmd, kubectl_cmd))
+
     # Do cleanup scenario
     print("Deleting Akri configuration: {}".format(shared_test_code.DEBUG_ECHO_NAME))
     os.system("sudo {} delete akric {}".format(kubectl_cmd, shared_test_code.DEBUG_ECHO_NAME))
