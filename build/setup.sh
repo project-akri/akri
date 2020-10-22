8194cb70b776806de836f9e717ee97e3f7190e1b
Brian Fjeldstad <bfjelds@microsoft.com>
2020-10-22 15:37:13 -0700

    Try installing rustup if needed [SAME VERSION]

--- build/setup.sh
+++ build/setup.sh
@@ -3,14 +3,9 @@
 # exit on failures
 set -e
 
-
-apt_dependencies="git curl libssl-dev pkg-config libudev-dev libv4l-dev"
-
 echo "User: $(whoami)"
 
-echo "Install rustfmt"
-rustup component add rustfmt
-
+apt_dependencies="git curl libssl-dev pkg-config libudev-dev libv4l-dev"
 echo "Install dependencies: $apt_dependencies"
 which sudo > /dev/null 2>&1
 if [ "$?" -eq "0" ];
@@ -24,3 +19,20 @@ else
     apt install -y $apt_dependencies
 fi
 
+which rustup > /dev/null 2>&1
+if [ "$?" -ne "0" ];
+then
+    which sudo > /dev/null 2>&1
+    if [ "$?" -eq "0" ];
+    then
+        echo "Install rustup"
+        sudo curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.41.0
+    else
+        echo "Install rustup"
+        curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.41.0
+    fi
+fi
+echo "Install rustfmt"
+rustup component add rustfmt
+
+exit 0
