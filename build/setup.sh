b98a8c568df201feb453c7667fda72935abf48bb
Brendan Burns <brendan.d.burns@gmail.com>
2020-10-22 11:26:35 -0700

    Add magic incantation and exit on errors.

--- build/setup.sh
+++ build/setup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# exit on failures
+set -e
+
 
 apt_dependencies="git curl libssl-dev pkg-config libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstrtspserver-1.0-dev libges-1.0-dev libudev-dev libv4l-dev"
 
