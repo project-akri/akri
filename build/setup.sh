170c790a4b96265b4defb9b6652875ca0b782d9c
bfjelds <bfjelds@microsoft.com>
2020-10-22 17:45:45 -0700

    Try installing rustup if needed in setup.sh [SAME VERSION] (#52)
    * Try installing rustup if needed [SAME VERSION]
    
    * we cannot depend on set -e because we are depending on 'which' failures in the script
    
    * add more error checks
    
    * be consistent with quotes

--- build/setup.sh
+++ build/setup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# exit on failures
-set -e
-
-
-apt_dependencies="git curl libssl-dev pkg-config libudev-dev libv4l-dev"
-
 echo "User: $(whoami)"
 
-echo "Install rustfmt"
-rustup component add rustfmt
-
+apt_dependencies="git curl libssl-dev pkg-config libudev-dev libv4l-dev"
 echo "Install dependencies: $apt_dependencies"
 which sudo > /dev/null 2>&1
 if [ "$?" -eq "0" ];
@@ -23,4 +15,36 @@ else
     apt update
     apt install -y $apt_dependencies
 fi
+if [ "$?" -ne "0" ]; then
+    echo "Failed to apt install: $apt_dependencies"
+    exit $?
+fi
 
+which rustup > /dev/null 2>&1
+if [ "$?" -ne "0" ];
+then
+    which sudo > /dev/null 2>&1
+    if [ "$?" -eq "0" ];
+    then
+        echo "Install rustup"
+        sudo curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.41.0
+    else
+        echo "Install rustup"
+        curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.41.0
+    fi
+    if [ "$?" -ne "0" ]; then
+        echo "Failed to install rustup"
+        exit $?
+    fi
+else
+    echo "Found rustup"
+fi
+
+echo "Install rustfmt"
+rustup component add rustfmt
+if [ "$?" -ne "0" ]; then
+    echo "Failed to install rustfmt"
+    exit $?
+fi
+
+exit 0
