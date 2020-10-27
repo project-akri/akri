744f4ce5ad7c890a346401392a6b3db14082bfc8
bfjelds <bfjelds@microsoft.com>
2020-10-27 12:32:08 -0700

    Enable pull_request for branch PRs, pull_request_target for fork PRs [SAME VERSION] [IGNORE INTERMEDIATE BUILDS] (#64)
    * enable pull_request for branch PRs, pull_request_target for fork PRs
    
    * try to understand why workflows are running twice
    
    * try to understand why workflows are running twice
    
    * minimize changes to make comparisons easier

--- .github/actions/build-intermediate/main.js
+++ .github/actions/build-intermediate/main.js
@@ -35,7 +35,7 @@ async function shell_cmd(cmd) {
         if (core.getInput('github_event_name') == 'release') push_containers = 1;
         else if (core.getInput('github_event_name') == 'push' && 
                 core.getInput('github_ref') == 'refs/heads/main') push_containers = 1;
-        else if (core.getInput('github_event_name') == 'pull_request_target' && 
+        else if (core.getInput('github_event_name').startsWith('pull_request') && 
                 core.getInput('github_event_action') == 'closed' && 
                 core.getInput('github_ref') == 'refs/heads/main' && 
                 core.getInput('github_merged') == 'true') push_containers = 1;
