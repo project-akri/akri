a78278b16d36269d41fd91bf675568f569c3d780
bfjelds <bfjelds@microsoft.com>
2021-02-25 16:38:03 -0800

    Simplify containers makefile (#256)
    * simplify makefile for containers
    
    * add for all containers
    
    * fix indent
    
    * bump version
    
    * try to fix indentation

---
+++ build/akri-rust-containers.mk
@@ -0,0 +1,52 @@
+
+# Create set of targets for rust builds
+define add_rust_targets
+  $(info compiling akri-$(1)) # logged when doing $(call ...)
+  $$(info evaluating akri-$(1)) # logged when doing $(eval $(call ...))
+
+  akri-$(1): akri-build akri-docker-$(1)
+  akri-docker-$(1): $(1)-build $(1)-docker-per-arch $(1)-docker-multi-arch-create $(1)-docker-multi-arch-push
+  $(1)-build: $(1)-build-amd64 $(1)-build-arm32 $(1)-build-arm64
+  $(1)-docker-per-arch: $(1)-docker-per-arch-amd64 $(1)-docker-per-arch-arm32 $(1)-docker-per-arch-arm64
+
+  $(1)-build-amd64:
+  ifeq (1, ${BUILD_AMD64})
+	docker build $(CACHE_OPTION) -f $(DOCKERFILE_DIR)/Dockerfile.$(2) . -t $(PREFIX)/$(2):$(LABEL_PREFIX)-$(AMD64_SUFFIX) --build-arg PLATFORM=$(AMD64_SUFFIX) --build-arg CROSS_BUILD_TARGET=$(AMD64_TARGET)
+  endif
+  $(1)-build-arm32:
+  ifeq (1, ${BUILD_ARM32})
+	docker build $(CACHE_OPTION) -f $(DOCKERFILE_DIR)/Dockerfile.$(2) . -t $(PREFIX)/$(2):$(LABEL_PREFIX)-$(ARM32V7_SUFFIX) --build-arg PLATFORM=$(ARM32V7_SUFFIX) --build-arg CROSS_BUILD_TARGET=$(ARM32V7_TARGET)
+  endif
+  $(1)-build-arm64:
+  ifeq (1, ${BUILD_ARM64})
+	docker build $(CACHE_OPTION) -f $(DOCKERFILE_DIR)/Dockerfile.$(2) . -t $(PREFIX)/$(2):$(LABEL_PREFIX)-$(ARM64V8_SUFFIX) --build-arg PLATFORM=$(ARM64V8_SUFFIX) --build-arg CROSS_BUILD_TARGET=$(ARM64V8_TARGET)
+  endif
+
+  $(1)-docker-per-arch-amd64:
+  ifeq (1, ${BUILD_AMD64})
+	docker push $(PREFIX)/$(2):$(LABEL_PREFIX)-$(AMD64_SUFFIX)
+  endif
+  $(1)-docker-per-arch-arm32:
+  ifeq (1, ${BUILD_ARM32})
+	docker push $(PREFIX)/$(2):$(LABEL_PREFIX)-$(ARM32V7_SUFFIX)
+  endif
+  $(1)-docker-per-arch-arm64:
+  ifeq (1, ${BUILD_ARM64})
+	docker push $(PREFIX)/$(2):$(LABEL_PREFIX)-$(ARM64V8_SUFFIX)
+  endif
+
+  $(1)-docker-multi-arch-create:
+  ifeq (1, ${BUILD_AMD64})
+	$(ENABLE_DOCKER_MANIFEST) docker manifest create --amend $(PREFIX)/$(2):$(LABEL_PREFIX) $(PREFIX)/$(1):$(LABEL_PREFIX)-$(AMD64_SUFFIX)
+  endif
+  ifeq (1, ${BUILD_ARM32})
+	$(ENABLE_DOCKER_MANIFEST) docker manifest create --amend $(PREFIX)/$(2):$(LABEL_PREFIX) $(PREFIX)/$(1):$(LABEL_PREFIX)-$(ARM32V7_SUFFIX)
+  endif
+  ifeq (1, ${BUILD_ARM64})
+	$(ENABLE_DOCKER_MANIFEST) docker manifest create --amend $(PREFIX)/$(2):$(LABEL_PREFIX) $(PREFIX)/$(1):$(LABEL_PREFIX)-$(ARM64V8_SUFFIX)
+  endif
+
+  $(1)-docker-multi-arch-push:
+	$(ENABLE_DOCKER_MANIFEST) docker manifest push $(PREFIX)/$(2):$(LABEL_PREFIX)
+
+endef
