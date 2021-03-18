30fe40f4022c3105e454f9f2f85fc02854c34512
Kate Goldenring <kate.goldenring@microsoft.com>
2021-03-18 09:27:11 -0700

    use full agent in e2e tests and make helm value for full default to false

--- test/run-end-to-end.py
+++ test/run-end-to-end.py
@@ -31,7 +31,7 @@ def main():
     print("Providing Akri Helm chart with CRI args: {}".format(cri_args))
     extra_helm_args = shared_test_code.get_extra_helm_args()
     print("Providing Akri Helm chart with extra helm args: {}".format(extra_helm_args))
-    helm_install_command = "helm install akri {} --set debugEcho.enabled=true --set debugEcho.name={} --set agent.allowDebugEcho=true {} {} --debug ".format(helm_chart_location, shared_test_code.DEBUG_ECHO_NAME, cri_args, extra_helm_args)
+    helm_install_command = "helm install akri {} --set agent.full=true --set debugEcho.enabled=true --set debugEcho.name={} --set agent.allowDebugEcho=true {} {} --debug ".format(helm_chart_location, shared_test_code.DEBUG_ECHO_NAME, cri_args, extra_helm_args)
     print("Helm command: {}".format(helm_install_command))
     os.system(helm_install_command)
     
