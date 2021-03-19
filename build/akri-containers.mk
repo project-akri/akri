5a55e5de653b4ada0611261f19b778ae10704021
Kate Goldenring <kate.goldenring@microsoft.com>
2021-03-19 09:00:01 -0700

    build full agent for e2e tests, remove echos

--- build/akri-containers.mk
+++ build/akri-containers.mk
@@ -5,9 +5,11 @@ include build/akri-rust-containers.mk
 include build/akri-dotnet-containers.mk
 include build/akri-python-containers.mk
 
+#
+# Functions for building Agent with or without Discovery Handlers
+#
 # Build the Agent without any Discovery Handlers embedded
 define agent_build_slim
-	echo agent slim && \
 	PKG_CONFIG_ALLOW_CROSS=1 cross build --release --target=$(1) --manifest-path agent/Cargo.toml
 endef
 
@@ -19,11 +21,6 @@ define agent_build_with_features
 	mv target/$(1)/release/agent target/$(1)/release/${FULL_AGENT_EXECUTABLE_NAME}
 endef
 
-# Build all Akri components except the Agent
-define build_all_but_agent
-	echo here && \
-	PKG_CONFIG_ALLOW_CROSS=1 cross build --release --target=$(1) --workspace --exclude agent 
-endef
 #
 #
 # INSTALL-CROSS: install cargo cross building tool:
