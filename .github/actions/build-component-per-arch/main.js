667d453bd2eb532e4d92d5bfdffd25c86f703158
harrisontin <harrisontin@microsoft.com>
2023-03-31 15:56:35 -0700

    update rust to 1.68.2 and tarpaulin to 0.25.1
    Signed-off-by: harrisontin <harrisontin@microsoft.com>

--- .github/actions/build-component-per-arch/main.js
+++ .github/actions/build-component-per-arch/main.js
@@ -67,7 +67,7 @@ async function shell_cmd(cmd) {
 
         if (core.getInput('build_rust') == '1') {
             console.log(`Install Rust`)
-            child_process.execSync(`curl https://sh.rustup.rs | sh -s -- -y --default-toolchain=1.63.0`);
+            child_process.execSync(`curl https://sh.rustup.rs | sh -s -- -y --default-toolchain=1.68.2`);
             const bindir = `${process.env.HOME}/.cargo/bin`;
             process.env.PATH = `${process.env.PATH}:${bindir}`;
 
