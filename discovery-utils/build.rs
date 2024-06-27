3f02e7bc1bd63af30cecd000fd9449b4ce69dc33
Kate Goldenring <kate.goldenring@fermyon.com>
2024-06-27 14:29:11 -0700

    fix: add proto build flag to allow option proto fields
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- discovery-utils/build.rs
+++ discovery-utils/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     tonic_build::configure()
         .out_dir("./src/discovery")
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["proto/discovery.proto"], &["proto"])
         .expect("failed to compile protos");
 }
